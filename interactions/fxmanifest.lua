6f0dfd9e677041621672ee8415139d314273fe4f
QuixQB <89683722+QuixQB@users.noreply.github.com>
2024-02-25 12:02:12 +1100

    Add files via upload

---
+++ interactions/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version "cerulean"
+
+games { "gta5" }
+
+description "NoPixel Interactions"
+
+version "0.1.0"
+
+files {
+    "assets/*.png",
+}
+
+server_scripts { "build/sv_*.js" }
+client_scripts { "build/cl_*.js" }
